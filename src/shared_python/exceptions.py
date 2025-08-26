036c7f674bbbca1ad2aaff8067b91c979ae7d587
nuniesmith <nunie.smith01@gmail.com>
2025-08-26 09:13:29 -0400

    refactor: simplify import statements in exception, logging, types, and utils modules

--- src/shared_python/exceptions.py
+++ src/shared_python/exceptions.py
@@ -1,11 +1,1 @@
-"""Bridge module for exceptions."""
-from importlib import import_module as _im
-_impl = _im("exceptions")
-globals().update({k: v for k, v in _impl.__dict__.items() if not k.startswith("_")})
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
-from __future__ import annotations
-try:
-    from fks_shared_python import exceptions as _m  # type: ignore
-except Exception:  # pragma: no cover
-    import exceptions as _m  # type: ignore
-from exceptions import *  # type: ignore  # noqa: F401,F403
+from fks_shared_python.exceptions import *  # type: ignore  # noqa: F401,F403
--- src/shared_python/utils.py
+++ src/shared_python/utils.py
@@ -1,11 +1,1 @@
-"""Bridge module for utility functions."""
-from importlib import import_module as _im
-_impl = _im("utils")
-globals().update({k: v for k, v in _impl.__dict__.items() if not k.startswith("_")})
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
-from __future__ import annotations
-try:
-    from fks_shared_python import utils as _m  # type: ignore
-except Exception:  # pragma: no cover
-    import utils as _m  # type: ignore
-from utils import *  # type: ignore  # noqa: F401,F403
+from fks_shared_python.utils import *  # type: ignore  # noqa: F401,F403
