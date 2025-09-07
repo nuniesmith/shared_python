460ac326a16f5f6c3749557e3d79626d98cfcd36
nuniesmith <nunie.smith01@gmail.com>
2025-09-07 16:07:54 -0400

    chore: sync submodule

--- src/shared_python/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Backward compatibility alias for ``shared_python`` (deprecated)."""
-import sys as _sys, importlib as _importlib
-import shared_python as _real
-from shared_python import *  # noqa: F401,F403
-__all__ = _real.__all__  # type: ignore
-__path__ = _real.__path__
-for _m in ["config","logging","exceptions","types","utils","risk","metrics","simulation"]:
-    _sys.modules[f"shared_python.{_m}"] = _importlib.import_module(f"shared_python.{_m}")
